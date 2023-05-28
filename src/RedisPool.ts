6704e485c9466777f932668da9f7d1c9420f1c6f
xue mufan <xuemf@ffcs.cn>
2023-05-28 21:49:31 +0800

    refactor: 重构缓存机制

---
+++ src/RedisPool.ts
@@ -0,0 +1,50 @@
+// 一份简单的缓存池
+import Redis from 'ioredis';
+export interface RedisConfig { 
+  port: number
+  host: string
+  password: string
+  index: number
+}
+
+export default class RedisPool {
+  private _pool: Redis[] = [];
+  private _redisConfig: RedisConfig
+
+  private async _prepareRedisClient(number = 5) {
+    const { port, host, password, index = 0 } = this._redisConfig
+    const redisClient = new Redis(this._redisConfig);
+    for (let i = 0; i < number; i++) {
+      const redisClient = new Redis(Number(port), host, {
+        password,
+        retryStrategy(times) {
+          const delay = Math.min(times * 50, 2000)
+          return delay
+        },
+      })
+      await redisClient.select(index)
+      this._pool.push(redisClient)
+    }
+  }
+  public constructor(config: RedisConfig) {
+    this._redisConfig = config
+    // 初始化 10 个 redis 连接
+    this._prepareRedisClient(10)
+  }
+
+  public async getRedisClient() {
+    if (this._pool.length) {
+      return this._pool.pop() as Redis;
+    } else {
+      await this._prepareRedisClient(5)
+    }
+
+    const redisClient = new Redis(this._redisConfig);
+    await redisClient.select(Number(this._redisConfig.index));
+    return redisClient;
+  }
+
+  public async releaseRedisClient(redisClient: Redis) {
+    this._pool.push(redisClient);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { Logger, IPluginMiddleware, IBasicAuth, IStorageManager, PluginOptions } from '@verdaccio/types';
 import {Router, Request, Response, NextFunction, Application} from 'express';
 import { CustomConfig } from '../types/index';
-import redis from 'ioredis'
+import type { RedisConfig } from './RedisPool'
+import RedisPool from './RedisPool';
 
 const parseJSON = (mayBeJSON: unknown, initial = {}) => { 
   try {
@@ -11,34 +12,21 @@ const parseJSON = (mayBeJSON: unknown, initial = {}) => {
   }
 }
 
-interface RedisConfig { 
-  port: number
-  host: string
-  password: string
-  index: number
-}
-
 export default class VerdaccioMiddlewarePlugin implements IPluginMiddleware<CustomConfig> {
   public logger: Logger;
   public foo: string;
   public redisConfig: RedisConfig
+  private redisPool: RedisPool
 
   public constructor(config: CustomConfig, options: PluginOptions<CustomConfig>) {
     this.foo = config.foo !== undefined ? config.strict_ssl : true;
     this.logger = options.logger;
     this.redisConfig = config.redis as RedisConfig
+    this.redisPool = new RedisPool(this.redisConfig)
   }
 
   private async _getRedisClient() { 
-    const { port, host, password, index = 0 } = this.redisConfig
-    const redisClient = new redis(Number(port), host, {
-      password,
-      retryStrategy(times) {
-        const delay = Math.min(times * 50, 2000)
-        return delay
-      },
-    })
-    await redisClient.select(Number(index))
+    const redisClient = await this.redisPool.getRedisClient()
     return redisClient
   }
 
@@ -47,7 +35,6 @@ export default class VerdaccioMiddlewarePlugin implements IPluginMiddleware<Cust
       auth: IBasicAuth<CustomConfig>,
       _storage: IStorageManager<CustomConfig>,
   ): void {
-    const downloadCounts = {}
     const downloadInterceptor = Router()
     // 拦截和获取包相关的请求
     downloadInterceptor.get('/*/-/*', async (req: Request, res: Response, next: NextFunction) => {
@@ -65,7 +52,8 @@ export default class VerdaccioMiddlewarePlugin implements IPluginMiddleware<Cust
         const today = new Date(Date.now() + 8 * 60 * 60 * 1000)
           .toISOString()
           .slice(0, 10)
-        const todayDownloadCache = await redisClient.get(`download-${today}`)
+        const cacheKey = `download-${today}`
+        const todayDownloadCache = await redisClient.hget(cacheKey, packageName)
         const downloadTempJsonContent = parseJSON(todayDownloadCache)
         
         // 获取压缩包，例如：@verdaccio/monorepo-1.0.0.tgz
@@ -74,8 +62,8 @@ export default class VerdaccioMiddlewarePlugin implements IPluginMiddleware<Cust
         const packageAndVersion = tgzFile.split('.').slice(0, -1).join('.')
         const nameAndVersionArr = packageAndVersion
           .split('-')
-        //兼容版本中带有 - 的情况
-        // 找到第一个字符不仅仅是字符的位置
+        
+        // 找到第一个字符不仅仅是字符的位置，兼容版本中带有 - 的情况
         const firstIndexNotJustAZChars = nameAndVersionArr.findIndex(
           (char) => !/[a-zA-Z]/.test(char)
         ) || 0
@@ -85,19 +73,18 @@ export default class VerdaccioMiddlewarePlugin implements IPluginMiddleware<Cust
         ).join('-')
 
         // 记录下载次数
-        const packageRecord = downloadCounts[packageName]
+        const packageRecord = downloadTempJsonContent[packageVersion]
         if (packageRecord) {
-          const preNumber = packageRecord[packageVersion] || 0
-          packageRecord[packageVersion] = preNumber + 1
+          downloadTempJsonContent[packageVersion] = packageRecord + 1
         } else {
-          downloadCounts[packageName] = {}
-          downloadCounts[packageName][packageVersion] = 1
+          downloadTempJsonContent[packageVersion] = 1
         }
 
         // 将下载次数写入缓存
-        Object.assign(downloadTempJsonContent, downloadCounts)
-        await redisClient.set(
-          `download-${today}`,
+        Object.assign(downloadTempJsonContent, packageRecord)
+        await redisClient.hset(
+          cacheKey,
+          packageName,
           JSON.stringify(downloadTempJsonContent)
         )
       } catch (e) {
@@ -108,6 +95,5 @@ export default class VerdaccioMiddlewarePlugin implements IPluginMiddleware<Cust
     })
     
     app.use(downloadInterceptor)
-      
   }
 }
